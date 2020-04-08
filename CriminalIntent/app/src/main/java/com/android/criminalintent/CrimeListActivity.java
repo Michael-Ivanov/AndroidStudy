4f354f26da0f565c5e95ed10ef39761ca64069a2
Michael Ivanov <rattys@yandex.ru>
2020-04-08 21:53:33 +0300

    Project CriminalIntent added

---
+++ CriminalIntent/app/src/main/java/com/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package com.android.criminalintent;
+
+import androidx.fragment.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
